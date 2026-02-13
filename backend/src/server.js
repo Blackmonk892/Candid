ef4d4912aa201c540e82074d600d46c291c1189b
Blackmonk892 <blackmonk2224@gmail.com>
2026-02-13 18:17:40 +0530

    created injest user login functions

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,38 @@
 import express from "express";
+import path from "path";
+import cors from "cors";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./lib/inngest.js";
+
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+
 const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions,
+  }),
+);
 
 app.get("/health", (req, res) => {
   res.status(200).json({
     message: "api is up and running",
   });
 });
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 const startServer = async () => {
   try {
