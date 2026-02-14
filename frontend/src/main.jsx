471ff20452f314d3b0c6765afb60818c545f1391
Blackmonk892 <blackmonk2224@gmail.com>
2026-02-14 15:54:48 +0530

    made the homepage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,6 +13,8 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Add your Clerk Publishable Key to the .env file");
 }
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
